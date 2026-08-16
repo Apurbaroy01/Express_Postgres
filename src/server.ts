32a0c66d5c06f22da8cc32c825da355f835543e1
Apurbaroy01 <apurbaroy00011@gmail.com>
2026-08-16 22:26:07 +0600

    fix

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,14 @@
 import express, { type Application, type Request, type Response } from "express";
+import {Pool} from "pg";
 const app: Application = express()
 const port = 5000
 
 app.use(express.json());
+
+
+const pool = new Pool({
+    connectionString:"postgresql://neondb_owner:npg_P7MSOnVsU2Tv@ep-withered-tree-ay8poq77-pooler.c-5.us-east-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
+})
 
 app.get('/', (req: Request, res: Response) => {
     res.status(200).json({ message: 'Hello World!' })
