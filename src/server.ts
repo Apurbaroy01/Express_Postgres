55e2b6a690f61926dc781501e7b4a23f0455fd09
Apurbaroy01 <apurbaroy00011@gmail.com>
2026-08-16 22:42:02 +0600

    fix: add database initialization and user table creation in server.ts

--- src/server.ts
+++ src/server.ts
@@ -10,6 +10,30 @@ const pool = new Pool({
     connectionString:"postgresql://neondb_owner:npg_P7MSOnVsU2Tv@ep-withered-tree-ay8poq77-pooler.c-5.us-east-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
 })
 
+const initDB = async () => {
+    const client = await pool.connect();
+    try {
+        await client.query(`
+            CREATE TABLE IF NOT EXISTS users (
+                id SERIAL PRIMARY KEY,
+                name VARCHAR(20) NOT NULL,
+                email VARCHAR(20) UNIQUE NOT NULL,
+                password VARCHAR(20) NOT NULL,
+                age INT NOT NULL,
+                is_active BOOLEAN DEFAULT true,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+
+            )
+        `);
+        console.log('Table created successfully');
+    } catch (err) {
+        console.error('Error creating table:', err);
+    }
+};
+
+initDB().catch(console.error);
+
 app.get('/', (req: Request, res: Response) => {
     res.status(200).json({ message: 'Hello World!' })
 });
