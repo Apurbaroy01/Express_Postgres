4467aad065f497d20922555efddcff72ab4b1c4e
Apurbaroy01 <apurbaroy00011@gmail.com>
2026-08-18 22:03:34 +0600

    fix: refactor server to use config module for database connection and port, implement CRUD operations for users

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,15 @@
 import express, { type Application, type Request, type Response } from "express";
 import { Pool } from "pg";
-import "dotenv/config";
+import config from "./config";
+
 const app: Application = express()
-const port = 5000
+const port = config.port;
 
 app.use(express.json());
 
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
+    connectionString: config.connection_string,
 })
 
 const initDB = async () => {
@@ -36,7 +37,7 @@ initDB().catch(console.error);
 
 
 // create user
-app.post("/", async (req: Request, res: Response) => {
+app.post("/user/api", async (req: Request, res: Response) => {
     const { name, email, password, age } = req.body;
 
     try {
@@ -51,6 +52,79 @@ app.post("/", async (req: Request, res: Response) => {
     } catch (error: any) {
         console.error('Error creating user:', error);
         res.status(500).json({ message: 'Error creating user' });
+    }
+});
+
+// get all users
+app.get("/user/api", async (req: Request, res: Response) => {
+    try {
+        const result = await pool.query(`
+            SELECT * FROM users
+            `);
+        res.status(200).json({ success: true, message: 'Users fetched successfully', users: result.rows });
+    } catch (error) {
+        console.error('Error fetching users:', error);
+        res.status(500).json({ message: 'Error fetching users' });
+    }
+});
+
+// get single user
+app.get("/user/api/:id", async (req: Request, res: Response) => {
+    const { id } = req.params;
+    try {
+        const result = await pool.query(`
+            SELECT * FROM users
+            WHERE id = $1
+            `, [id]);
+
+        if (!result.rows.length) {
+            return res.status(404).json({ success: false, message: 'User not found' });
+        }
+        res.status(200).json({ success: true, message: 'User fetched successfully', user: result.rows[0] });
+    } catch (error) {
+        console.error('Error fetching user:', error);
+        res.status(500).json({ message: 'Error fetching user' });
+    }
+});
+
+// update user
+app.put("/user/api/:id", async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const { name, email, password, age } = req.body;
+    try {
+        const result = await pool.query(`
+            UPDATE users
+            SET name =COALESCE($1, name), email = $2, password = $3, age = $4
+            WHERE id = $5
+            RETURNING *
+            `, [name, email, password, age, id]);
+        if (!result.rows.length) {
+            return res.status(404).json({ success: false, message: 'User not found' });
+        }
+        res.status(200).json({ success: true, message: 'User updated successfully', user: result.rows[0] });
+    } catch (error) {
+        console.error('Error updating user:', error);
+        res.status(500).json({ message: 'Error updating user' });
+    }
+});
+
+// DELETE USER
+app.delete("/user/api/:id", async (req: Request, res: Response) => {
+    const { id } = req.params;
+    try {
+        const result = await pool.query(`
+            DELETE FROM users
+            WHERE id = $1
+            RETURNING *
+            `, [id]);
+
+        if (!result.rows.length) {
+            return res.status(404).json({ success: false, message: 'User not found' });
+        }
+        res.status(200).json({ success: true, message: 'User deleted successfully', user: result.rows[0] });
+    } catch (error) {
+        console.error('Error deleting user:', error);
+        res.status(500).json({ message: 'Error deleting user' });
     }
 });
 
